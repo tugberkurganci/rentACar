15ce2329bdeb07e51c6eb806ff57b3d7b90c8827
RamiKoco <ramikoco333433@gmail.com>
2023-12-15 10:11:28 +0300

    Color validation added

--- pair-3/src/main/java/com/tobeto/pair3/services/dtos/requests/CreateColorRequest.java
+++ pair-3/src/main/java/com/tobeto/pair3/services/dtos/requests/CreateColorRequest.java
@@ -1,10 +1,12 @@
 package com.tobeto.pair3.services.dtos.requests;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateColorRequest {
-    private int id;
+
+    @Size(min=2)
     private String name;
 
 
--- pair-3/src/main/java/com/tobeto/pair3/services/dtos/responses/GetAllColorResponse.java
+++ pair-3/src/main/java/com/tobeto/pair3/services/dtos/responses/GetAllColorResponse.java
@@ -1,8 +1,12 @@
 package com.tobeto.pair3.services.dtos.responses;
 
-import com.tobeto.pair3.entities.Brand;
-import com.tobeto.pair3.entities.Color;
 
+import com.tobeto.pair3.entities.Color;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class GetAllColorResponse {
     private int id;
     private String name;
